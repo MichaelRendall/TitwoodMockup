0886c11a9c0dad735e11d0bb4e5e923d82cd8e94
Michael Rendall <michaelrendallmail@gmail.com>
2021-07-18 23:24:10 +0100

    created footer for web, still to add media queries

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,31 +4,66 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <footer className={`${classes.footer} padding`}>
-      <div>
-        <p>
-          <FontAwesomeIcon icon={["fas", "mobile-alt"]} /> 0141 552 4800
-        </p>
+      <div className={classes.footerRow}>
+        <div>
+          <p>
+            <FontAwesomeIcon
+              icon={["fas", "mobile-alt"]}
+              className={classes.contactIcon}
+            />
+            0141 552 4800
+          </p>
 
-        <p>
-          <FontAwesomeIcon icon={["fas", "envelope"]} />{" "}
-          info@titwoodlawntennisclub.co.uk
-        </p>
+          <p>
+            <FontAwesomeIcon
+              icon={["fas", "envelope"]}
+              className={classes.contactIcon}
+            />
+            info@titwoodlawntennisclub.co.uk
+          </p>
+        </div>
+        <div>
+          <ul className={classes.ul}>
+            <li>
+              <FontAwesomeIcon
+                icon={["fab", "facebook-f"]}
+                className={classes.mediaIcon}
+              />
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={["fab", "twitter"]}
+                className={classes.mediaIcon}
+              />
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={["fab", "instagram"]}
+                className={classes.mediaIcon}
+              />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>
+            Titwood Lawn Tennis Club,
+            <br />
+            Glencairn Gardens,
+            <br />
+            Glasgow, G41 4LN
+          </p>
+        </div>
       </div>
-      <div>
-        <FontAwesomeIcon icon={["fab", "facebook-f"]} />
-        <FontAwesomeIcon icon={["fab", "twitter"]} />
-        <FontAwesomeIcon icon={["fab", "instagram"]} />
-      </div>
-      <div>
-        <p>
-          Titwood Lawn Tennis Club,
-          <br />
-          Glencairn Gardens,
-          <br />
-          Glasgow,
-          <br />
-          G41 4LN
-        </p>
+      <div className={classes.footerRow}>
+        <div>
+          <small>© 2020 Titwood Lawn Tennis Club</small>
+        </div>
+        <div className="centerText">
+          <small>Privacy Policy</small>
+        </div>
+        <div>
+          <small>Designed &amp; Developed by Michael Rendall</small>
+        </div>
       </div>
     </footer>
   );
