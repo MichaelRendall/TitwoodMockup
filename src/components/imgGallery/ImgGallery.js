98e4cba0a3a79987bb040a5d9a31d47ac9519dba
Michael Rendall <michaelrendallmail@gmail.com>
2021-09-18 14:02:12 +0100

    added image gallery

---
+++ src/components/imgGallery/ImgGallery.js
@@ -0,0 +1,64 @@
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/scss/image-gallery.scss";
+import "./ImgGalleryOverride.scss";
+
+import one from "../../assets/main-background.jpg";
+import two from "../../assets/court4.jpg";
+import three from "../../assets/courts-2and3.jpg";
+import four from "../../assets/court3.jpg";
+import five from "../../assets/brodie.jpg";
+import six from "../../assets/alan.jpg";
+import seven from "../../assets/champions-board.jpg";
+
+import oneThumb from "../../assets/thumbnails/main-background.jpg";
+import twoThumb from "../../assets/thumbnails/court4.jpg";
+import threeThumb from "../../assets/thumbnails/courts-2and3.jpg";
+import fourThumb from "../../assets/thumbnails/court3.jpg";
+import fiveThumb from "../../assets/thumbnails/brodie.jpg";
+import sixThumb from "../../assets/thumbnails/alan.jpg";
+import sevenThumb from "../../assets/thumbnails/champions-board.jpg";
+
+const images = [
+  {
+    original: one,
+    thumbnail: oneThumb,
+  },
+  {
+    original: two,
+    thumbnail: twoThumb,
+  },
+  {
+    original: three,
+    thumbnail: threeThumb,
+  },
+  {
+    original: four,
+    thumbnail: fourThumb,
+  },
+  {
+    original: five,
+    thumbnail: fiveThumb,
+  },
+  {
+    original: six,
+    thumbnail: sixThumb,
+  },
+  {
+    original: seven,
+    thumbnail: sevenThumb,
+  },
+];
+
+const ImgGallery = () => {
+  return (
+    <div className="section-container">
+      <ImageGallery
+        items={images}
+        thumbnailPosition="top"
+        showPlayButton={false}
+      />
+    </div>
+  );
+};
+
+export default ImgGallery;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
+import PageHeader from "../components/UI/PageHeader";
+import ImgGallery from "../components/imgGallery/ImgGallery";
 
 const Gallery = () => {
   useEffect(() => {
     document.title = "Gallery | Titwood Tennis";
   });
-  return "images here";
+  return (
+    <div className="padding">
+      <PageHeader title="Gallery" />
+      <ImgGallery />
+    </div>
+  );
 };
 
 export default Gallery;
